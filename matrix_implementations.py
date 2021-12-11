1bad245719347b2ffceb2ce31bada1fa89323b91
Gustav Gyrst <gugy@itu.dk>
2021-12-11 13:57:45 +0100

    strassens added

--- matrix_implementations.py
+++ matrix_implementations.py
@@ -385,12 +385,91 @@ def recursive_multiplication_write_through(A: Matrix, B: Matrix, m: int)->Matrix
 
 
 
-def strassen(A: Matrix, B: Matrix, m: int)->Matrix:
+
+def strassen(A: Matrix, B: Matrix) -> Matrix:
+    
+    n = A.rows()
+    
+    if n == 1:
+        C = A[0]*B[0]
+        return C
+    
+    else:
+        
+        #Making a C responding to the current size of A in the current recursion level
+        C = Matrix(A.rows(), A.rows())
+        
+        #Cutting our copy of C into four sections
+        C00 = C[:n//2,:n//2]
+        C01 = C[:n//2,n//2:]
+        C10 = C[n//2:,:n//2]
+        C11 = C[n//2:,n//2:]
+        
+        
+        # P1 = A00 + A11
+        P1 = A[:n//2,:n//2] + A[n//2:,n//2:]
+        # P2 = A10 + A11
+        P2 = A[n//2:,:n//2] + A[n//2:,n//2:]
+        # P3 = A00
+        P3 = A[:n//2,:n//2]
+        # P4 = A11
+        P4 = A[n//2:,n//2:]
+        # P5 = A00 + A01
+        P5 = A[:n//2,:n//2] + A[:n//2,n//2:]
+        # P6 = A10 - A00
+        P6 = A[n//2:,:n//2] - A[:n//2,:n//2]
+        # P7 = A01 - A11
+        P7 = A[:n//2,n//2:] - A[n//2:,n//2:]
+        
+        # Q1 = B00 + B11
+        Q1 = B[:n//2,:n//2] + B[n//2:,n//2:]
+        # Q2 = B00
+        Q2 = B[:n//2,:n//2]
+        # Q3 = B01 - B11 
+        Q3 = B[:n//2,n//2:] - B[n//2:,n//2:]
+        # Q4 = B10 - B00
+        Q4 = B[n//2:,:n//2] - B[:n//2,:n//2]
+        # Q5 = B11
+        Q5 = B[n//2:,n//2:]
+        # Q6 = B00 + B01
+        Q6 = B[:n//2,:n//2] + B[:n//2,n//2:]
+        # Q7 = B10 + B11
+        Q7 = B[n//2:,:n//2] + B[n//2:,n//2:]
+            
+        # Then compute Mi = Pi*Qi by a recursive application of the function
+        M1 = strassen(P1,Q1)
+        M2 = strassen(P2,Q2)
+        M3 = strassen(P3,Q3)
+        M4 = strassen(P4,Q4)
+        M5 = strassen(P5,Q5)
+        M6 = strassen(P6,Q6)
+        M7 = strassen(P7,Q7)
+        
+        # Following the recipe from the slides:
+        
+        C00 += M1 + M4 - M5 + M7
+        C01 += M3 + M5
+        C10 += M2 + M4
+        C11 += M1 - M2 + M3 + M6
+
+        return C
+
+
+def strassen_main(A: Matrix, B: Matrix, m: int)->Matrix:
     """
     Computes C=AB using Strassen's algorithm. The structure ought
     to be similar to the copying recursive algorithm. The parameter
     m controls when the routine falls back to a cubic algorithm, as
     the subproblem size satisfies n <= m.
     """
-    raise NotImplementedError('Fill in the implementation')
+    n = A.rows()
+    
+    if n <= m:
+        return elementary_multiplication(A,B)
+    else:
+        return strassen(A,B)
+
+        
+
+
 
