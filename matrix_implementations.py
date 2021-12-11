afce8fd5f3d5303fa4341d52fe168b6b21f7b8fe
Gustav Gyrst <gugy@itu.dk>
2021-12-11 15:07:40 +0100

    runs

--- matrix_implementations.py
+++ matrix_implementations.py
@@ -340,24 +340,24 @@ def recursive_multipliction_write_through_fun(A: Matrix, B: Matrix, C: Matrix) -
         return C
     
     else:
-        #M0 C upper left                        a00             b00             c00
+        #M0 C upper left                                        a00             b00             c00
         a00b00 = recursive_multipliction_write_through_fun(A[:n//2,:n//2], B[:n//2,:n//2], C[:n//2,:n//2])
-        #M1 C upper left                        a01             b10             c00
+        #M1 C upper left                                        a01             b10             c00
         a01b10 = recursive_multipliction_write_through_fun(A[:n//2,n//2:], B[n//2:,:n//2], C[:n//2,:n//2])
         
-        #M2 C upper right                       a00             b01             c01
+        #M2 C upper right                                       a00             b01             c01
         a00b01 = recursive_multipliction_write_through_fun(A[:n//2,:n//2], B[:n//2,n//2:], C[:n//2,n//2:])
-        #M3 C upper right                       a01             b11             c01
+        #M3 C upper right                                       a01             b11             c01
         a01b11 = recursive_multipliction_write_through_fun(A[:n//2,n//2:], B[n//2:,n//2:], C[:n//2,n//2:])
         
-        #M4 C lower left                        a10             b00             c10
+        #M4 C lower left                                        a10             b00             c10
         a10b00 = recursive_multipliction_write_through_fun(A[n//2:,:n//2], B[:n//2,:n//2], C[n//2:,:n//2])
-        #M5 C lower left                        a11             b10             c10
+        #M5 C lower left                                        a11             b10             c10
         a11b10 = recursive_multipliction_write_through_fun(A[n//2:,n//2:], B[n//2:,:n//2], C[n//2:,:n//2])
         
-        #M6 C lower right                       a10             b01             c11
+        #M6 C lower right                                       a10             b01             c11
         a10b01 = recursive_multipliction_write_through_fun(A[n//2:,:n//2], B[:n//2,n//2:], C[n//2:,n//2:])
-        #M7 C lower right                       a11             b11             c11
+        #M7 C lower right                                       a11             b11             c11
         a11b11 = recursive_multipliction_write_through_fun(A[n//2:,n//2:], B[n//2:,n//2:], C[n//2:,n//2:])
 
         return C
