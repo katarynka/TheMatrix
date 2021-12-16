6c81677fa6160d0d56296def1b231b71647c1376
Gustav Gyrst <gugy@itu.dk>
2021-12-16 10:47:27 +0100

    tiled change to n/8

--- experiments/horse_race/tiled_matrix_multiplication_e.py
+++ experiments/horse_race/tiled_matrix_multiplication_e.py
@@ -39,6 +39,13 @@ def benchmark_tiled(f: FunType , n_list: list, s:int, N: int)->np.ndarray: #N is
             f(A,B,s)
         
         print("show_time!")
+        
+        # Calculating the Dynamic S value so that it divides the matrix into 8 pieces (optimal).
+        if (n>8):
+            s = n/8
+        else:
+            s = n/2
+            
         for j in range(N):
             M[n,j] = measure(lambda: f(A,B,s))
             print("time:")
